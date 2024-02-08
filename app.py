817de832431985b97a214dd9828ffa8fbcac8bcc
PJ <prince11jose@hotmail.com>
2024-02-08 13:06:54 +0530

    /ping

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/health')
 def health_check():
-    return jsonify({"status": "up"})
+    return {"status": "up"}
+
+@app.route('/ping')
+def ping():
+    return "pong"
 
 if __name__ == '__main__':
     app.run(debug=True)
